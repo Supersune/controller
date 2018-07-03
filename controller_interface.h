7c93aa000a6d309dda91e6b62450229c0bb90d49
Erectus Maximus <xderial@gmail.com>
2018-07-03 22:34:16 +0200

    Formatted code

--- controller_interface.h
+++ controller_interface.h
@@ -1,13 +1,12 @@
 namespace controller_interface
 {
-class Controller
-{
-public:
-virtual bool init(hardware_interface *robotHW,
-ros::NodeHandle &nh);
-virtual void starting();
-virtual void update();
-virtual void stopping();
-};
-
+    class Controller
+    {
+    public:
+        virtual bool init(hardware_interface *robotHW,
+        ros::NodeHandle &nh);
+        virtual void starting();
+        virtual void update();
+        virtual void stopping();
+    };
 }
--- src/my_controller.h
+++ src/my_controller.h
@@ -1,19 +1,20 @@
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <pluginlib/class_list_macros.h>
-namespace my_controller_ns {
-class MyControllerClass: public
-controller_interface::Controller<hardware_interface::PositionJointInterface
->
+
+
+namespace my_controller_ns
 {
-public:
-bool init(hardware_interface::PositionJointInterface* hw,
-ros::NodeHandle &n);
-void update(const ros::Time& time, const ros::Duration& period);
-void starting(const ros::Time& time);
-void stopping(const ros::Time& time);
-private:
-hardware_interface::JointHandle joint_;
-double init_pos_;
-};
+    class MyControllerClass: public
+    controller_interface::Controller<hardware_interface::PositionJointInterface>
+    {
+    public:
+        bool init(hardware_interface::PositionJointInterface* hw, ros::NodeHandle &n);
+        void update(const ros::Time& time, const ros::Duration& period);
+        void starting(const ros::Time& time);
+        void stopping(const ros::Time& time);
+        private:
+        hardware_interface::JointHandle joint_;
+        double init_pos_;
+    };
 }
